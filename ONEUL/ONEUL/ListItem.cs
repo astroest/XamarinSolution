185f13a3a276117d91cbd6b3c1da3a7377443634
hes <es001206@naver.com>
2020-12-14 22:54:30 +0900

    초기 커밋.

---
+++ ONEUL/ONEUL/ListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONEUL
+{
+    class ListItem
+    {
+        public string title;
+        public int time;
+
+        public ListItem(string title, int time)
+        {
+            this.title = title;
+            this.time = time;
+        }
+        
+    }
+}
---
+++ ONEUL/ONEUL/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ONEUL
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<ListItem> listItems;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            listItems = new List<ListItem>();
+            listItems.Add(new ListItem("운동", 10));
+            listItems.Add(new ListItem("강의", 23));
+            listItems.Add(new ListItem("코딩", 12));
+
+            BindingContext = this;
+
+        }
+    }
+}
