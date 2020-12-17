acd1f4cf9b4b65b39d8f72d0c6518ac116c74367
hes <es001206@naver.com>
2020-12-17 18:58:22 +0900

    Add WritePage

--- ONEUL/ONEUL/MainPage.xaml.cs
+++ ONEUL/ONEUL/MainPage.xaml.cs
@@ -5,26 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ONEUL.ViewModel;
 
 namespace ONEUL
 {
     public partial class MainPage : TabbedPage
     {
+        readonly MainPageViewModel viewModel;
         private List<ListItem> listItems;
 
         public MainPage()
         {
             InitializeComponent();
 
+            BindingContext = viewModel = new MainPageViewModel();
+
             listItems = new List<ListItem>
             {
                 new ListItem("운동", 10),
                 new ListItem("강의", 23),
-                new ListItem("코딩", 12)
+                new ListItem("코딩", 12),
+                new ListItem("독서", 8)
             };
 
-            main_list.ItemsSource = listItems;
-            main_list.BindingContext = listItems;
+            main_list.ItemsSource = this.listItems;
 
         }
     }
---
+++ ONEUL/ONEUL/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ONEUL.ViewModel
+{
+    public class MainPageViewModel : ContentPage
+    {
+        public Command addOneul { get; }
+        public MainPageViewModel()
+        {
+            addOneul = new Command(OnAddOneul);
+        }
+
+        private async void OnAddOneul(object obj)
+        {
+            await Shell.Current.GoToAsync(nameof(WritePage));
+            throw new NotImplementedException();
+        }
+    }
+}
